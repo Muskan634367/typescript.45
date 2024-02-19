5f729e22ad2a0cccaa5e2138e61eb91e84c61369
Muskan634367 <160433920+Muskan634367@users.noreply.github.com>
2024-02-19 22:10:23 +0500

    EXERSICE.11and12
    typescript

---
+++ app.js
@@ -0,0 +1,14 @@
+// // Assignment Solution
+// //exercise 11
+// let names: string [] = ["Laiba","Urooj","Malaika","Hamza"];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// //exercise 12
+var names = ["Laiba", "Urooj", "Malaika", "Hamza"];
+var message = "Do you like to play football?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
